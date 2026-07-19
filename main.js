346176ea4a8d1fa38511ff0a0ea62f9de18e9b06
LukitoRD123 <lukitoopal@gmail.com>
2026-07-19 19:25:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -90,26 +90,3 @@
   });
 
 })();     
-/* ======================================
-   TERASS ECO TECHNOLOGY V3.3.2
-   Lazy Loading Premium
-====================================== */
-
-document.addEventListener("DOMContentLoaded", function () {
-
-  const images = document.querySelectorAll("img");
-
-  images.forEach(function(img){
-
-    if(!img.hasAttribute("loading")){
-      img.setAttribute("loading","lazy");
-    }
-
-    if(!img.hasAttribute("decoding")){
-      img.setAttribute("decoding","async");
-    }
-
-  });
-
-});
-
