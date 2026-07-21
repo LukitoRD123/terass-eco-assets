94fd8e20ec7c7ae2e85580b86d7b47f3cce38bdb
LukitoRD123 <lukitoopal@gmail.com>
2026-07-21 15:50:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -54,60 +54,6 @@ header.classList.remove("sticky-active");
 
 
 /* ===========================================================
-   DARK MODE
-=========================================================== */
-
-function darkMode(){
-
-const button =
-document.querySelector(".dark-toggle");
-
-
-if(!button) return;
-
-
-const saved =
-localStorage.getItem("terass-dark");
-
-
-if(saved==="active"){
-
-document.body.classList.add("dark-mode");
-
-}
-
-
-button.addEventListener("click",function(){
-
-
-document.body.classList.toggle("dark-mode");
-
-
-if(
-document.body.classList.contains("dark-mode")
-){
-
-localStorage.setItem(
-"terass-dark",
-"active"
-);
-
-}else{
-
-localStorage.removeItem(
-"terass-dark"
-);
-
-}
-
-
-});
-
-}
-
-
-
-/* ===========================================================
    BACK TO TOP
 =========================================================== */
 
