b717ebe2ce54a6244be226bac9156c0d9ddc4362
LukitoRD123 <lukitoopal@gmail.com>
2026-07-21 15:48:51 +0800

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,1 +1,51 @@
+/* ===========================================================
+   DARK MODE
+=========================================================== */
 
+function darkMode(){
+
+const button =
+document.querySelector(".dark-toggle");
+
+
+if(!button) return;
+
+
+const saved =
+localStorage.getItem("terass-dark");
+
+
+if(saved==="active"){
+
+document.body.classList.add("dark-mode");
+
+}
+
+
+button.addEventListener("click",function(){
+
+
+document.body.classList.toggle("dark-mode");
+
+
+if(
+document.body.classList.contains("dark-mode")
+){
+
+localStorage.setItem(
+"terass-dark",
+"active"
+);
+
+}else{
+
+localStorage.removeItem(
+"terass-dark"
+);
+
+}
+
+
+});
+
+}
