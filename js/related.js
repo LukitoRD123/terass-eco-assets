9435341cde8575bc7c5032febc6571d699403235
LukitoRD123 <lukitoopal@gmail.com>
2026-07-22 10:58:59 +0800

    Create related.js

---
+++ js/related.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+const related=document.querySelector("#related-post");
+
+
+if(!related)return;
+
+
+related.innerHTML=`
+
+<h3>
+Artikel Terkait
+</h3>
+
+<p>
+Artikel teknologi terbaru lainnya akan muncul di sini.
+</p>
+
+`;
+
+});
