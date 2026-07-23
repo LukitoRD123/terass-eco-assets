b02e96021cc9ec9917baa9b02de3c2826ae6f0a8
LukitoRD123 <lukitoopal@gmail.com>
2026-07-23 12:06:33 +0800

    Update reading.js

--- js/reading.js
+++ js/reading.js
@@ -15,8 +15,46 @@ let scroll=window.scrollY-top;
 let progress=(scroll/(height-window.innerHeight))*100;
 
 
-document.querySelector("#reading-progress")
-.style.width=progress+"%";
+const bar=document.querySelector("#reading-progress");
+
+if(bar){
+
+bar.style.width=progress+"%";
+
+}
+
+
+});
+
+/*
+TERASS ECO READING TIME
+*/
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+const content=document.querySelector(".post-body");
+
+const output=document.querySelector("#reading-time");
+
+
+if(!content || !output)return;
+
+
+
+const words=content.innerText
+.trim()
+.split(/\s+/)
+.length;
+
+
+const minutes=Math.ceil(words/200);
+
+
+
+output.innerHTML=
+"Waktu baca: "+minutes+" menit";
 
 
 });
